09b279ae18168c7c3ab13f218d585e962509d68d
rajesh-rajagopal <rajeshr@megam.io>
2017-09-28 10:15:39 +0530

    .

--- client.go
+++ client.go
@@ -93,18 +93,18 @@ fmt.Println("*****cli conn******")
 }
 
 // Connect creates an OVSDB connection and returns and OvsdbClient
-func Connect(ipAddr string ) (*OvsdbClient, error) {
+func Connect(ipAddr string, port int) (*OvsdbClient, error) {
 	if ipAddr == "" {
 		ipAddr = DefaultAddress
 	}
 
 fmt.Println("-----enter------")
 
-	// if port <= 0 {
-	// 	port = DefaultPort
-	// }
+	if port <= 0 {
+		port = DefaultPort
+	}
 
-	target := fmt.Sprintf("%s", ipAddr)
+	target := fmt.Sprintf("%s:%d", ipAddr, port)
         fmt.Println("target")
         fmt.Println(target)
         fmt.Println("target")
